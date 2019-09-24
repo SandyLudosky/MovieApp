2052d6aa24313065b12b78c7d852c6b671863661
Sandy Ludosky <sandyludosky@gmail.com>
2019-09-24 13:48:35 +0200

    placeholder poster

--- app/config/constants.js
+++ app/config/constants.js
@@ -2,4 +2,5 @@ export const Constants = {
     IMAGE_BASE_URL: 'http://image.tmdb.org/t/p/w185', 
     IMDB_BASE_URL: 'https://www.imdb.com/title', 
     PLACEHOLDER: 'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-128.png', 
+    PLACEHOLDER_POSTER: 'https://causeofaction.org/wp-content/uploads/2013/09/Not-available.gif'
 }
